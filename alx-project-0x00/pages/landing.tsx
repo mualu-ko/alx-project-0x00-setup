88123242ceea432572252f53f5fb406aac611fb8
mualu-ko <kmwaluko.wambua@gmail.com>
2025-09-15 15:39:24 +0300

    more fixes

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,12 +6,12 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title="Click Me" size="large" shape="rounded-full" />
+      <Button title="Click Me" size="large" shape="rounded-full" styles="" />
       <Card />
-      <Button title="Submit" size="medium" shape="rounded-md" />
+      <Button title="Submit" size="medium" shape="rounded-md" styles="" />
       <Card />
-      <Button title="Learn More" size="small" shape="rounded-sm" />
-      <Button title="Get Started" size="large" shape="rounded-lg" />
+      <Button title="Learn More" size="small" shape="rounded-sm" styles="" />
+      <Button title="Get Started" size="large" shape="rounded-lg" styles="" />
     </div>
   )
 }
